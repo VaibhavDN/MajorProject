7b42f0adb0ce7cef20f4d3464809da151f3666eb
Vaibhav <deshbandhu1008@gmail.com>
2020-11-11 21:18:07 +0530

    Added working drawer

--- app/src/main/java/com/prabalbhavishya/cars/MainActivity.kt
+++ app/src/main/java/com/prabalbhavishya/cars/MainActivity.kt
@@ -1,63 +1,78 @@
 package com.prabalbhavishya.cars
 
-import android.content.ComponentName
 import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.content.pm.ResolveInfo
-import android.graphics.drawable.Drawable
-import android.os.Bundle
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.layout_bottomsheet.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        supportActionBar?.hide()
         setContentView(R.layout.activity_main)
 
-        val ic1: ImageView = findViewById(R.id.imageView)
-        val ic2: ImageView = findViewById(R.id.imageView2)
-        val ic3: ImageView = findViewById(R.id.imageView3)
-        val ic4: ImageView = findViewById(R.id.imageView4)
+        var bottomSheet = findViewById<ConstraintLayout>(R.id.layoutBottomSheet)
+        var bottomSheetBehavior = BottomSheetBehavior.from(layoutBottomSheet)
+        bottomSheetBehavior.addBottomSheetCallback(object:BottomSheetBehavior.BottomSheetCallback(){
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when(newState){
+                    BottomSheetBehavior.STATE_EXPANDED -> {
+                        Toast.makeText(applicationContext, "Expanded", Toast.LENGTH_SHORT).show()
+                    }
+                    BottomSheetBehavior.STATE_COLLAPSED -> {
+                        Toast.makeText(applicationContext, "Collapsed", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
 
-        ic1.setImageDrawable(
-            get_app_icon(
-                this,
-                "com.android.chrome",
-                "com.google.android.apps.chrome.Main"
-            )
-        )
-        ic2.setImageDrawable(
-            get_app_icon(
-                this,
-                "com.File.manager",
-                "com.File.manager.SplashActivity"
-            )
-        )
-        ic3.setImageDrawable(
-            get_app_icon(
-                this,
-                "com.android.deskclock",
-                "com.android.deskclock.DeskClock"
-            )
-        )
-        ic4.setImageDrawable(
-            get_app_icon(
-                this,
-                "com.google.android.youtube",
-                "com.google.android.apps.youtube.app.honeycomb.phone.NewVersionAvailableActivity"
-            )
-        )
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                //Do nothing
+            }
 
+        })
+
+        var myAppsList = Array<Int>(100){0}
+        for (i in 0..99 step 1){
+            myAppsList[i] = i
+        }
+
+        var recyclerView = findViewById<RecyclerView>(R.id.appDrawer_RecyclerView)
+        var  gridLayoutManager = GridLayoutManager(this, 5)
+        recyclerView.layoutManager = gridLayoutManager
+        recyclerView.addItemDecoration(DividerItemDecoration(this, LinearLayoutManager.VERTICAL))
+
+        var recyclerViewAdapter = RecyclerViewAdapter(myAppsList)
+        recyclerView.adapter = recyclerViewAdapter
     }
 
-    fun get_app_icon(context: Context, app_name: String, app_activity_name: String): Drawable {
-        val pm: PackageManager = context.packageManager
-        val intent = Intent()
-        intent.component = ComponentName(app_name, app_activity_name)
-        val resolveinfo: ResolveInfo = pm.resolveActivity(intent, 0)
-        return resolveinfo.loadIcon(pm)
-    }
+    class RecyclerViewAdapter(var myAppsList: Array<Int>) :
+        RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+            override fun onCreateViewHolder(
+                parent: ViewGroup,
+                viewType: Int
+            ): RecyclerViewAdapter.ViewHolder {
+                val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_appicon, parent, false)
+                return ViewHolder(view)
+            }
+            override fun onBindViewHolder(holder: RecyclerViewAdapter.ViewHolder, position: Int) {
+                holder.appIconTextView.text = myAppsList[position].toString()
+            }
+
+            override fun getItemCount(): Int {
+                return myAppsList.size
+            }
+
+            class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+                var appIconTextView: TextView = itemView.findViewById(R.id.appIcon_TextView)
+            }
+        }
 }
