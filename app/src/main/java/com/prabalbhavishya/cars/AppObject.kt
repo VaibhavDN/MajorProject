0b8699d2989331b0c24714518864570a45a77a82
Vaibhav <deshbandhu1008@gmail.com>
2020-11-13 22:49:43 +0530

    Added app launch functionality

---
+++ app/src/main/java/com/prabalbhavishya/cars/AppObject.kt
@@ -0,0 +1,21 @@
+package com.prabalbhavishya.cars
+
+import android.graphics.drawable.Drawable
+
+class AppObject(newAppName: String, newPackageName: String, newAppIcon: Drawable) {
+    private var appName : String = newAppName
+    private var packageName : String = newPackageName
+    private var appIcon : Drawable = newAppIcon
+
+    fun get_appName() : String{
+        return this.appName
+    }
+
+    fun get_packageName() : String{
+        return this.packageName
+    }
+
+    fun get_appIcon() : Drawable{
+        return this.appIcon
+    }
+}
--- app/src/main/java/com/prabalbhavishya/cars/MainActivity.kt
+++ app/src/main/java/com/prabalbhavishya/cars/MainActivity.kt
@@ -1,18 +1,27 @@
 package com.prabalbhavishya.cars
 
+import android.app.ActionBar
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.pm.ResolveInfo
+import android.content.res.Resources
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.DisplayMetrics
+import android.view.*
 import android.widget.*
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.layout_appicon.view.*
 import kotlinx.android.synthetic.main.layout_bottomsheet.*
 
 class MainActivity : AppCompatActivity() {
@@ -40,21 +49,40 @@ class MainActivity : AppCompatActivity() {
 
         })
 
-        var myAppsList = Array<Int>(100){0}
-        for (i in 0..99 step 1){
-            myAppsList[i] = i
-        }
+        var myAppsList : ArrayList<AppObject> = fetchAppsList()
 
         var recyclerView = findViewById<RecyclerView>(R.id.appDrawer_RecyclerView)
         var  gridLayoutManager = GridLayoutManager(this, 5)
         recyclerView.layoutManager = gridLayoutManager
-        recyclerView.addItemDecoration(DividerItemDecoration(this, LinearLayoutManager.VERTICAL))
+        //recyclerView.addItemDecoration(DividerItemDecoration(this, LinearLayoutManager.HORIZONTAL))
 
         var recyclerViewAdapter = RecyclerViewAdapter(myAppsList)
         recyclerView.adapter = recyclerViewAdapter
     }
 
-    class RecyclerViewAdapter(var myAppsList: Array<Int>) :
+    private fun fetchAppsList() : ArrayList<AppObject>{
+        var intent = Intent(Intent.ACTION_MAIN, null)
+        intent.addCategory(Intent.CATEGORY_LAUNCHER)
+        var untreatedAppList : List<ResolveInfo> = applicationContext.packageManager.queryIntentActivities(intent, 0)
+
+        var finalAppsList: ArrayList<AppObject> = ArrayList()
+
+        for(app : ResolveInfo in untreatedAppList){
+            var appName : String = app.activityInfo.loadLabel(packageManager).toString()
+            var packageName : String = app.activityInfo.packageName
+            var appIcon : Drawable = app.activityInfo.loadIcon(packageManager)
+            var appObject : AppObject = AppObject(appName, packageName, appIcon)
+
+            if(appName == "CARS"){
+                continue
+            }
+
+            finalAppsList.add(appObject)
+        }
+        return finalAppsList
+    }
+
+    class RecyclerViewAdapter(var myAppsList: ArrayList<AppObject>) :
         RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
             override fun onCreateViewHolder(
                 parent: ViewGroup,
@@ -63,8 +91,21 @@ class MainActivity : AppCompatActivity() {
                 val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_appicon, parent, false)
                 return ViewHolder(view)
             }
+
             override fun onBindViewHolder(holder: RecyclerViewAdapter.ViewHolder, position: Int) {
-                holder.appIconTextView.text = myAppsList[position].toString()
+                //var width = Resources.getSystem().displayMetrics.widthPixels
+                //var height = Resources.getSystem().displayMetrics.heightPixels
+                //holder.appIconImageView.layoutParams = ConstraintLayout.LayoutParams(width/50, height/50)
+                holder.appIconImageView.setImageDrawable(myAppsList[position].get_appIcon())
+
+                holder.appIconTextView.text = myAppsList[position].get_appName()
+                holder.appIconConstraintLayout.setOnClickListener {
+                    Toast.makeText(holder.context, myAppsList[position].get_packageName(), Toast.LENGTH_SHORT).show()
+                    val appLaunchIntent = holder.context.packageManager.getLaunchIntentForPackage(myAppsList[position].get_packageName())
+                    if(appLaunchIntent != null){
+                        holder.context.applicationContext.startActivity(appLaunchIntent)
+                    }
+                }
             }
 
             override fun getItemCount(): Int {
@@ -72,7 +113,10 @@ class MainActivity : AppCompatActivity() {
             }
 
             class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+                var context = itemView.context
+                var appIconConstraintLayout: ConstraintLayout = itemView.findViewById(R.id.layoutConstraint_appIcon)
                 var appIconTextView: TextView = itemView.findViewById(R.id.appIcon_TextView)
+                var appIconImageView: ImageView = itemView.findViewById(R.id.appIcon_ImageView)
             }
         }
 }
