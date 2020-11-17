ef3e2b02d9afdfc8d2a3edf7915acfd0ff2cc596
Vaibhav <deshbandhu1008@gmail.com>
2020-11-17 11:42:59 +0530

    Fixed handleWindowVisibility: no activity for token android.os.BinderProxy bug

--- app/src/main/java/com/prabalbhavishya/cars/AppsUsageStats.kt
+++ app/src/main/java/com/prabalbhavishya/cars/AppsUsageStats.kt
@@ -8,6 +8,7 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.provider.Settings
+import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -29,49 +30,41 @@ class UsageStats : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_usage_stats)
 
+        Log.d("AppsUsageStats: ", "Reading file")
+        val usageStatsTextView = findViewById<TextView>(R.id.usageStats_TextView1)
+        var textInFile: String
+
+        //Read usage stats from disk
+        try {
+            val path = getExternalFilesDir("UsageDir")
+            val file = File(path, "appUsageData.csv")
+            val fileInputStream = FileInputStream(file)
+            textInFile = fileInputStream.readBytes().toString(Charset.defaultCharset())
+            fileInputStream.close()
+            Log.d("AppsUsageStats: ", "Read successful")
+        }
+        catch (e: Exception){
+            textInFile = e.toString()
+            Log.d("AppsUsageStats: ", "Read failed")
+            requestPermissionFromUser()
+        }
+        usageStatsTextView.text = textInFile
+    }
+
+    fun requestPermissionFromUser(){
         val appOps: AppOpsManager = getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager
 
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
-            if (appOps.unsafeCheckOpNoThrow(
-                    AppOpsManager.OPSTR_GET_USAGE_STATS,
-                    android.os.Process.myUid(),
-                    packageName
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                startActivityForResult(
-                    Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS),
-                    MY_USAGE_REQUEST_CODE
-                )
+            if (appOps.unsafeCheckOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS, android.os.Process.myUid(), packageName) != PackageManager.PERMISSION_GRANTED) {
+                startActivityForResult(Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS), MY_USAGE_REQUEST_CODE)
+                Log.d("AppsUsageStats: ", "Asking permission Q")
             }
-        } else if (appOps.checkOpNoThrow(
-                AppOpsManager.OPSTR_GET_USAGE_STATS,
-                android.os.Process.myUid(),
-                packageName
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
+        } else if (appOps.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS, android.os.Process.myUid(), packageName) != PackageManager.PERMISSION_GRANTED) {
             /*val permissionString = Array<String>(1){""}
             permissionString[0] = android.Manifest.permission.PACKAGE_USAGE_STATS
             ActivityCompat.requestPermissions(this, permissionString, MY_USAGE_REQUEST_CODE)*/
-            startActivityForResult(
-                Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS),
-                MY_USAGE_REQUEST_CODE
-            )
-        } else {
-            val usageStatsTextView = findViewById<TextView>(R.id.usageStats_TextView1)
-            var textInFile: String
-
-            //Read usage stats from disk
-            try {
-                val path = getExternalFilesDir("UsageDir")
-                val file = File(path, "appUsageData.csv")
-                val fileInputStream = FileInputStream(file)
-                textInFile = fileInputStream.readBytes().toString(Charset.defaultCharset())
-                fileInputStream.close()
-            }
-            catch (e: Exception){
-                textInFile = e.toString()
-            }
-            usageStatsTextView.text = textInFile
+            startActivityForResult(Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS), MY_USAGE_REQUEST_CODE)
+            Log.d("AppsUsageStats: ", "Asking permission legacy")
         }
     }
 
@@ -84,10 +77,10 @@ class UsageStats : AppCompatActivity() {
         if (requestCode == MY_USAGE_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
                 Toast.makeText(applicationContext, "Permission Granted", Toast.LENGTH_SHORT).show()
-                return
             }
-
-            Toast.makeText(applicationContext, "Permission Denied", Toast.LENGTH_SHORT).show()
+            else{
+                Toast.makeText(applicationContext, "Permission Denied", Toast.LENGTH_SHORT).show()
+            }
             finish()
         }
     }
