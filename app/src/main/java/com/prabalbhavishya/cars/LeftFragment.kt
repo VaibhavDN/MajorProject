2ad4e9bb6bd69ee66adb08ca0f8da2de2a908b22
Heat-Ray <heatray55@gmail.com>
2020-11-24 17:07:16 +0530

    SVG support added
    Signed-off-by: Heat-Ray <heatray55@gmail.com>

---
+++ app/src/main/java/com/prabalbhavishya/cars/LeftFragment.kt
@@ -0,0 +1,22 @@
+package com.prabalbhavishya.cars
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * An example full-screen fragment that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+class LeftFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_left, container, false)
+    }
+
+}
