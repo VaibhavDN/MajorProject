c0318790066cd577890cd2b4fd5240300b02c286
Heat-Ray <heatray55@gmail.com>
2020-11-27 21:48:13 +0530

    App deletion recommendation
    Signed-off-by: Heat-Ray <heatray55@gmail.com>

--- app/src/main/java/com/prabalbhavishya/cars/App_Prediction.kt
+++ app/src/main/java/com/prabalbhavishya/cars/App_Prediction.kt
@@ -1,6 +1,8 @@
 package com.prabalbhavishya.cars
 
+import android.app.usage.UsageStatsManager
 import android.content.Context
+import android.util.Log
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -14,5 +16,45 @@ class App_Prediction {
             appsReturned.add(appsFetched[i])
         }
         return appsReturned
+    }
+
+    fun RemoveAppList(context: Context): ArrayList<AppObject> {
+        val appsFetched = FetchAppsList().fetchList(context)
+        val time = Calendar.getInstance()
+        time.set(Calendar.DAY_OF_MONTH, -1)
+        val start = time.timeInMillis
+        val end = System.currentTimeMillis()
+
+        val us = context.getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
+        var stats = us.queryUsageStats(2, start, end)
+        val OldAppName = ArrayList<String>()
+        val OldUserAppName = ArrayList<AppObject>()
+        for (i in stats) {
+            val comp: Long = 0
+            if (i.totalTimeInForeground.equals(comp)) {
+                OldAppName.add(i.packageName)
+                Log.println(
+                    Log.VERBOSE,
+                    "pkg name : ",
+                    i.packageName + " " + i.totalTimeInForeground
+                )
+            }
+        }
+        for (i in appsFetched) {
+            if (i.get_packageName() in OldAppName) {
+                OldUserAppName.add(i)
+            }
+        }
+        if (OldUserAppName.size > 5) {
+            val rnd = Random()
+            val rndints = IntArray(5) { rnd.nextInt(OldUserAppName.size) }
+            val appsReturned = ArrayList<AppObject>()
+            for (i in rndints) {
+                appsReturned.add(appsFetched[i])
+            }
+            return appsReturned
+        }
+
+        return OldUserAppName
     }
 }
--- app/src/main/java/com/prabalbhavishya/cars/LeftFragment.kt
+++ app/src/main/java/com/prabalbhavishya/cars/LeftFragment.kt
@@ -1,22 +1,77 @@
 package com.prabalbhavishya.cars
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
 
 /**
  * An example full-screen fragment that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
 class LeftFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_left, container, false)
+        val view = inflater.inflate(R.layout.fragment_left, container, false)
+
+//        val intent = Intent(ACTION_USAGE_ACCESS_SETTINGS)
+//        startActivity(intent)
+
+        val removerecview = view.findViewById<RecyclerView>(R.id.removeapps)
+        val predApplist = App_Prediction().RemoveAppList(context!!)
+        val removeappadap = RemoveAppListAdapter(predApplist)
+        val grid = GridLayoutManager(context, 5)
+        removerecview.layoutManager = grid
+        removerecview.adapter = removeappadap
+        return view
+    }
+
+    class RemoveAppListAdapter(private var RemoveApplist: ArrayList<AppObject>) :
+        RecyclerView.Adapter<RemoveAppListAdapter.ViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
+                ViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.layout_predicted_appicon, parent, false)
+            return ViewHolder(view)
+        }
+
+        override fun getItemCount(): Int {
+            return RemoveApplist.size
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.appIconImageView.setImageDrawable(RemoveApplist[position].get_appIcon())
+
+            holder.appIconConstraintLayout.setOnClickListener {
+                val intent = Intent(Intent.ACTION_DELETE)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                intent.data = Uri.parse("package:" + RemoveApplist[position].get_packageName())
+                holder.context.applicationContext.startActivity(intent)
+                RemoveApplist.removeAt(position)
+                notifyItemRemoved(position)
+                notifyDataSetChanged()
+            }
+        }
+
+        class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            val context: Context = itemView.context
+            var appIconConstraintLayout: ConstraintLayout =
+                itemView.findViewById(R.id.layoutConstraint_appIcon)
+            var appIconImageView: ImageView = itemView.findViewById(R.id.appIcon_ImageView)
+        }
     }
 
 }
